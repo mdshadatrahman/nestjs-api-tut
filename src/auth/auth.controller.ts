aad48538b660532da1fab7154992b6335848610a
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-01-02 22:03:02 +0600

    dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { AuthService } from './auth.service';
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) { }
 
 	@Post('signup')
-	signup() {
+	signup(@Body() dto: any) {
+		console.log({
+			dto,
+		})
 		return this.authService.signup();
 	}
 
